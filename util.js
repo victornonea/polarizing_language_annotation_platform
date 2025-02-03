e1ddc0faa4b310e90de436e53ef1d54f6d042ccd
Victor-Andrei NONEA (94403) <victor_andrei.nonea@stud.acs.upb.ro>
2025-02-03 15:19:16 +0100

    init

---
+++ util.js
@@ -0,0 +1,52 @@
+function get_supabase_client() {
+    const { createClient } = supabase;
+    return createClient('https://jhhabujcthpkgyweeisr.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoaGFidWpjdGhwa2d5d2VlaXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMzE5NzcsImV4cCI6MjA1MjcwNzk3N30.nvwTBWdJ1IZGLN9Qubtoa5ve1pY5aO9HTJMIRRQX5Ts'
+    );
+}
+
+async function user_login(client, email, password) {
+    return await client.auth.signInWithPassword({
+        email: email,
+        password: password,
+    })
+}
+
+async function logout(client) {
+    await client.auth.signOut()
+}
+
+async function fetchArticleIds(client) {
+    const { data, error } = await client
+         .from('Articles')
+         .select('id');
+    
+    return data
+}
+
+async function fetchArticle(id, client) {
+    const { data, error } = await client
+         .from('Articles')
+         .select('text')
+         .eq('id', id);
+    
+    return data[0]
+}
+
+async function fetchFormQuestionsData(client) {
+    const { data, error } = await client
+        .from('FormQuestions')
+        .select('*, FormQuestionType(*)')
+    
+    return data
+}
+
+function randChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function setText(text, elemId) {
+    document.getElementById(elemId).innerText = text
+}
+
+export {get_supabase_client, user_login, logout, fetchArticleIds, randChoice, fetchArticle, fetchFormQuestionsData, setText}
